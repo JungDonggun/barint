074353e5327a6cb34a4de2bbeb5a1512f247e48b
JungDonggun <donggun.dev@gmail.com>
2020-08-04 20:31:15 +0900

    Add bluetooth module

--- app/src/main/java/com/example/barint/MainActivity.kt
+++ app/src/main/java/com/example/barint/MainActivity.kt
@@ -1,12 +1,86 @@
 package com.example.barint
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var app_bluetoothAdapter: BluetoothAdapter? = null
+    lateinit var pairedDevices: Set<BluetoothDevice>
+    val REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_address"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        app_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (app_bluetoothAdapter == null) {
+            System.out.println("This device doesn't support bluetooth")
+        }
+
+        if (!app_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+
+        }
+
+        pairedDeviceList()
+    }
+
+
+    fun pairedDeviceList() {
+        pairedDevices = app_bluetoothAdapter!!.bondedDevices
+        val list: ArrayList<BluetoothDevice> = ArrayList()
+
+        if (!pairedDevices.isEmpty()) {
+            for (device: BluetoothDevice in pairedDevices) {
+                list.add(device)
+                Log.i("DEVICE LIST =>", ""+device)
+            }
+        } else {
+            System.out.println("no paired devices found")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val device: BluetoothDevice = list[position]
+            val address: String = device.address
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(EXTRA_ADDRESS, address)
+            startActivity(intent)
+
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (requestCode === Activity.RESULT_OK) {
+                if (app_bluetoothAdapter!!.isEnabled) {
+                    System.out.println("Bluetooth has been enabled")
+                } else {
+                    System.out.println("Bluetooth has been disabled")
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                System.out.println("Bluetooth enabling has been canceled")
+            }
+        }
     }
 }
